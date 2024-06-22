00bf4ffb73e27c6e68070b2ba21b6770c242227c
ndpisme137 <nguyenphuc6a15.8@gmail.com>
2024-06-22 22:31:23 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <h1>Khanh PV</h1>
+    <ul>
+      <li>
+        <h3>Product A</h3>
+      </li>
+      <li>
+        <p>Price: 100</p>
+      </li>
+      <li>
+        Count: 3
+      </li>
+    </ul>
+    <h1>Khanh PV</h1>
+    <ul>
+      <li>
+        <h3>Product B</h3>
+      </li>
+      <li>
+        <p>Price: 200</p>
+      </li>
+      <li>
+        Count: 3
+      </li>
+    </ul>
+    <h1>Khanh PV</h1>
+    <ul>
+      <li>
+        <h3>Product c</h3>
+      </li>
+      <li>
+        <p>Price: 300</p>
+      </li>
+      <li>
+        Count: 3
+      </li>
+    </ul>
+  </main>
+</template>
